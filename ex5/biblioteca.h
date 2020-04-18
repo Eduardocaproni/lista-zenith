bdd9efc0a3332322ee1713f08bf0d0495f4eacbc
Eduardocaproni <62776112+Eduardocaproni@users.noreply.github.com>
2020-04-18 15:32:22 -0300

    Add files via upload

---
+++ ex5/biblioteca.h
@@ -0,0 +1,15 @@
+#ifndef BIBLIOTECA_H
+#define BIBLIOTECA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	float fmedia(int* vetor);
+	float fsoma(int* vetor);
+	float fdesviop(int* vetor, float media);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
